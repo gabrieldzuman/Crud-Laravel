ee6f2919cc09cef8b1f486de822874f1d90859d1
Gabriel Dzuman <83187468+gabrieldzuman@users.noreply.github.com>
2024-11-11 04:18:24 -0300

    Update SeriesCreated.php

--- app/Events/SeriesCreated.php
+++ app/Events/SeriesCreated.php
@@ -4,34 +4,24 @@ namespace App\Events;
 
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Broadcasting\PresenceChannel;
 use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
+#[ShouldBroadcast]
 class SeriesCreated
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
-    /**
-     * Create a new event instance.
-     *
-     * @return void
-     */
     public function __construct(
         public readonly string $seriesName,
         public readonly int $seriesId,
         public readonly int $seriesSeasonsQty,
-        public readonly int $seriesEpisodesPerSeason,
+        public readonly int $seriesEpisodesPerSeason
     ) {}
 
-    /**
-     * Get the channels the event should broadcast on.
-     *
-     * @return \Illuminate\Broadcasting\Channel|array
-     */
-    public function broadcastOn()
+    public function broadcastOn(): Channel
     {
         return new PrivateChannel('channel-name');
     }
