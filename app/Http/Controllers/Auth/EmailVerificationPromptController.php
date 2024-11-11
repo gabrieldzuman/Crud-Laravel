e88f7ff2d9f442199daeb8aedebf50661ed61f1d
Gabriel Dzuman <83187468+gabrieldzuman@users.noreply.github.com>
2024-11-11 04:25:01 -0300

    Update EmailVerificationPromptController.php

--- app/Http/Controllers/Auth/EmailVerificationPromptController.php
+++ app/Http/Controllers/Auth/EmailVerificationPromptController.php
@@ -5,19 +5,18 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\View\View;
 
 class EmailVerificationPromptController extends Controller
 {
     /**
      * Display the email verification prompt.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return mixed
      */
-    public function __invoke(Request $request)
+    public function __invoke(Request $request): View|RedirectResponse
     {
         return $request->user()->hasVerifiedEmail()
-                    ? redirect()->intended(RouteServiceProvider::HOME)
-                    : view('auth.verify-email');
+            ? redirect()->intended(RouteServiceProvider::HOME)
+            : view('auth.verify-email');
     }
 }
