5d6a33be95f62b1ca6cef3eff6952561845df8c9
Gabriel Dzuman <83187468+gabrieldzuman@users.noreply.github.com>
2024-12-20 04:59:08 -0300

    Update UsersController.php

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,26 +2,39 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UserFormRequest;
 use App\Models\User;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
 class UsersController
 {
+    /**
+     * Exibe o formulário para criar um novo usuário.
+     *
+     * @return \Illuminate\View\View View de criação de usuário.
+     */
     public function create()
     {
         return view('users.create');
     }
 
-    public function store(Request $request)
+    /**
+     * Armazena um novo usuário no banco de dados e realiza login.
+     *
+     * @param UserFormRequest $request Requisição validada com dados do formulário.
+     * @return \Illuminate\Http\RedirectResponse Redireciona para a página de séries após o login.
+     */
+    public function store(UserFormRequest $request)
     {
-        $data = $request->except(['_token']);
+        $data = $request->validated();
         $data['password'] = Hash::make($data['password']);
 
         $user = User::create($data);
+
         Auth::login($user);
 
-        return to_route('series.index');
+        return to_route('series.index')
+            ->with('mensagem.sucesso', "Bem-vindo, {$user->name}! Sua conta foi criada com sucesso.");
     }
 }
